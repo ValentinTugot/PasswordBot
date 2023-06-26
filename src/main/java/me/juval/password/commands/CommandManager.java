575a689750cd2cced90fd97d890742a52da3f3c2
ValentinTugot <valentin.tugot@gmail.com>
2023-06-26 20:39:29 +0200

    v0.3

--- src/main/java/me/juval/password/commands/CommandManager.java
+++ src/main/java/me/juval/password/commands/CommandManager.java
@@ -17,57 +17,69 @@ import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 public class CommandManager extends ListenerAdapter {
     public int randomInt(int range) {
         Random rand = new Random();
-        int randomNb = rand.nextInt(range);
-        return randomNb;
+        return rand.nextInt(range);
     }
 
     public String shuffledString(String str) {
         List<String> caracts = Arrays.asList(str.split(""));
         Collections.shuffle(caracts);
-        String ans = "";
+        StringBuilder ans = new StringBuilder(110);
         for (String caract : caracts)
-            ans = ans + caract;
-        return ans;
+            ans.append(caract);
+        return ans.toString();
     }
 
     public String selectChar(char[] liste, int range){
-        String ans = "";
+        StringBuilder ans = new StringBuilder();
         for (int i = 0; i < range; i++) {
             int randomIndex = randomInt(liste.length);
-            ans = ans + liste[randomIndex];
+            ans.append(liste[randomIndex]);
         }
-        return ans;
+        return ans.toString();
     }
 
-    public String genPassword() {
+    public String genPassword(int length) {
         char[] min = "abcdefghijklmnopqrstuvwxyz".toCharArray();
         char[] caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
         char[] number = "0123456789".toCharArray();
         char[] special = "&!.@?".toCharArray();
-        String prePassword = "";
-        prePassword = selectChar(min,8) + selectChar(caps,4) + selectChar(number,3) + selectChar(special,1);
 
-        String password = shuffledString(prePassword);
-        return password;
+        String prePassword = selectChar(min,length/2) + selectChar(caps,length/4) + selectChar(number,length/8) + selectChar(special,length/8);
+
+        return shuffledString(prePassword);
+
     }
 
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         String command = event.getName();
         if (command.equalsIgnoreCase("gen")) {
             OptionMapping plateformeOption = event.getOption("plateforme");
+            OptionMapping longeur = event.getOption("longeur");
+            assert plateformeOption != null;
             String plateforme = plateformeOption.getAsString();
-            String password = genPassword();
-            try {
-                event.reply("**" + plateforme.toUpperCase() + "**").queue();
-                Thread.sleep(1000L);
-                event.getChannel().sendMessage("||" + password + "||").queue();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            assert longeur != null;
+            int passLength = longeur.getAsInt();
+
+            if (passLength < 8 || passLength > 32){
+                event.reply("Longeur incorrect").queue();
+
+            }else{
+                String password = genPassword(passLength);
+                try {
+                    event.reply("**" + plateforme.toUpperCase() + "**").queue();
+                    Thread.sleep(1000L);
+                    event.getChannel().sendMessage("||" + password + "||").queue();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
+
         } else if (command.equalsIgnoreCase("password")) {
             OptionMapping plateformeOption = event.getOption("plateforme");
+            assert plateformeOption != null;
             String plateforme = plateformeOption.getAsString();
             OptionMapping pwdOption = event.getOption("password");
+            assert pwdOption != null;
             String password = pwdOption.getAsString();
             try {
                 event.reply("**" + plateforme.toUpperCase() + "**").queue();
@@ -81,10 +93,14 @@ public class CommandManager extends ListenerAdapter {
 
     public void onGuildReady(GuildReadyEvent event) {
         List<CommandData> commandData = new ArrayList<>();
+        // Options Gen Command
+        OptionData genPlateforme = new OptionData(OptionType.STRING, "plateforme", "Le nom de la plateforme associau MDP", true);
+        OptionData longeur = new OptionData(OptionType.INTEGER, "longeur", "Longeur du mot de passe (~ 8-32)", true);
+        // Options Password Command
         OptionData plateforme = new OptionData(OptionType.STRING, "plateforme", "Le nom de la plateforme associau MDP", true);
         OptionData password = new OptionData(OptionType.STRING, "password", "Entrez le mot de passe associé");
-                commandData.add(Commands.slash("gen", "Genun mot de passe").addOptions(new OptionData[] { plateforme }));
-        commandData.add(Commands.slash("password", "Ajoute un mot de passe deja existant").addOptions(new OptionData[] { plateforme, password }));
+        commandData.add(Commands.slash("gen", "Génère mot de passe").addOptions(genPlateforme,longeur));
+        commandData.add(Commands.slash("password", "Ajoute un mot de passe deja existant").addOptions(plateforme, password));
         event.getGuild().updateCommands().addCommands(commandData).queue();
     }
 }
